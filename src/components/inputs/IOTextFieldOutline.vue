738655d2a12304268fdf3406b0ded68c2a7b175d
tejas-encora <tejas.monteverdi@encora.com>
2023-10-17 15:08:14 -0400

    textfield

---
+++ src/components/inputs/IOTextFieldOutline.vue
@@ -0,0 +1,12 @@
+<template>
+  <md-outlined-text-field label="Label" value="Test Value"></md-outlined-text-field>
+</template>
+
+<script>
+import '@/assets/theme.css'
+
+export default {
+  name: "IOTextFieldOutline"
+};
+</script>
+
---
+++ src/stories/inputs/IOTextFieldOutline.stories.js
@@ -0,0 +1,18 @@
+import '@material/web/textfield/outlined-text-field.js'
+import IOTextFieldOutline from '@/components/inputs/IOTextFieldOutline.vue'
+import { action } from '@storybook/addon-actions';
+
+export default {
+  title: 'UI/Atoms/Inputs/Text/Outlined',
+  component: IOTextFieldOutline,
+  argTypes: {
+
+  },
+};
+
+export const TextField = {
+  render: () => ({
+    components: { IOTextFieldOutline },
+    template: '<IOTextFieldOutline>Click Me</IOTextFieldOutline>'
+  }),
+};
